8dab60fb0141ce2a5df50a8b51b53d2f563283e0
Piyush Pattanayak <5844258+PiyushPattanayak@users.noreply.github.com>
2026-06-09 01:39:50 -0700

    added player sprite, along with update and rotate, pygame groups

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import *
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,6 +10,16 @@ def main():
     
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    # Player is the name of the class, not an instance of it
+    # This must be done before any Player objects are created
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     # game loop
     while True:
@@ -19,7 +30,11 @@ def main():
                 return
         
         screen.fill("black")
+        updatable.update(dt)
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
